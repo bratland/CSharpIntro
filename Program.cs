cf7d49bf85b0f3016f031da62d462708972d5f4c
Anders Bratland <anders@bratland.se>
2018-07-01 20:16:25 +0200

    Petter

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace CSharpIntro
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World! Säger Petter");
         }
     }
 }
